371e15d9cea1a4182cc0ed0c8427231db4d21fd0
MarkClerigo <45374704+MarkClerigo@users.noreply.github.com>
2018-11-26 15:47:31 -0800

    Test Code File

---
+++ dynamic_input_Clerigo.py
@@ -0,0 +1,12 @@
+#accept input form user
+name = input("Enter your name:")
+print("Your name is: " + name)
+
+#accept numerical input -- int()
+fnum = int(input("Enter First Number: "))
+snum = int(input("Enter Second Number: "))
+
+to_int_value = int("10")
+
+print(fnum + snum)
+print(to_int_value ** 2)
