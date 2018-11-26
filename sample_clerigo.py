e1bd4ee0721debf6f93e6f2b9971759066444919
MarkClerigo <45374704+MarkClerigo@users.noreply.github.com>
2018-11-26 15:50:29 -0800

    Test Code File

---
+++ sample_clerigo.py
@@ -0,0 +1,34 @@
+# Declaring and Initialising variables in Python
+
+#String
+RoRO = "Loveteam"
+Ralph = "Pogi"
+Balbin = "Bobo"
+Intro = "Hello World"
+
+print(RoRO)
+print(Ralph)
+print(Balbin)
+print(Intro)
+
+#Arithmetic
+addition = 1+1
+subtraction = 1-1
+multiplication = 1*1
+division = 1/1
+modulus = 1%1
+
+print(addition)
+print(subtraction)
+print(multiplication)
+print(division)
+print(modulus)
+
+if addition > division:
+    print("Hi")
+    print("Kamusta ka?")
+elif division > subtraction:
+    print("Hello")
+    print("OKay lang")
+else:
+    print("Mamaya nalang")
