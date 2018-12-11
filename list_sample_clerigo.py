5257047051a99ca1cb2926afed3cd263b00d9ab6
MarkClerigo <45374704+MarkClerigo@users.noreply.github.com>
2018-12-10 19:36:56 -0800

    Test Commit File

---
+++ list_sample_clerigo.py
@@ -0,0 +1,16 @@
+list1 = ["Brian", "Dominic", "Hoobs", "Carter", "Roman"]
+list2 = [1,2,3,4,5,]
+list3 = [1.1,2.2,3.3,4.4,5.5]
+list4 = ["A","B","C","D","E"]
+
+for w in range(len(list1)):
+    print(list1[w])
+print("\n")
+for x in range(len(list2)):
+    print(list2[x])
+print("\n")
+for y in range(len(list3)):
+    print(list3[y])
+print("\n")
+for z in range(len(list4)):
+    print(list4[z])
