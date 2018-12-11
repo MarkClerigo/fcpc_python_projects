a8045a169826d8ed2905beff21ca5ac15813f61f
MarkClerigo <45374704+MarkClerigo@users.noreply.github.com>
2018-12-10 16:59:55 -0800

    Test Code

---
+++ activity_a_Clerigo.py
@@ -0,0 +1,20 @@
+#Variable
+val1 = 100
+print(val1)
+
+#Operators
+value1 = 10
+value2 = 20
+print(value1+value2)
+
+#Conditionals
+num1 = 10
+
+if (num1 > 5):
+     print("It is greater than 5")
+else:
+     print("It is not greater than 5")
+
+#Loops
+for x in range (1,10,2):
+    print("Mark")
