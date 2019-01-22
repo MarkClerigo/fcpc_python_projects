15d89c1fad75ffb5ff0da4a28a6751b6f2e091a1
MarkClerigo <45374704+MarkClerigo@users.noreply.github.com>
2019-01-21 19:01:19 -0800

    Test Commit File

---
+++ splice_clerigo.py
@@ -0,0 +1,13 @@
+#3 example of list splicing
+
+#Integer
+my_list = [1,2,3,4,5,6,7,8]
+print (my_list[0:6])
+
+#Float
+my_list = [1.5,2.3,3.1,4.6,5.7,6.4,7.2,8.9]
+print (my_list[3:6])
+
+#String
+my_list = ["Mark","Ralph","Janjan","Christian"]
+print (my_list[0:2])
