3821a7880d32d6935d8591340fc159d7f239e729
MarkClerigo <45374704+MarkClerigo@users.noreply.github.com>
2019-02-11 16:46:14 -0800

    Test Commit File

---
+++ list2d_clerigo.py
@@ -0,0 +1,10 @@
+list2d= [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+
+for numbers in list2d[0]:
+    print(numbers)
+
+for numbers in list2d[1]:
+    print(numbers)
+
+for numbers in list2d[2]:
+    print (numbers)
