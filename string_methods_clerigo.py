342284d0a90cb95b27400deb6c86244b13329d14
MarkClerigo <45374704+MarkClerigo@users.noreply.github.com>
2019-02-18 19:34:54 -0800

    Test Commit File

---
+++ string_methods_clerigo.py
@@ -0,0 +1,23 @@
+txt = "i'm, mark."
+
+x = txt.capitalize()
+
+print (x)
+
+txt = "Hello world"
+
+x = txt.split()
+
+print(x)
+
+txt = "I love programming"
+
+x = txt.replace("programming", "to sleep")
+
+print(x)
+
+txt = "I love eat, eating is my favorite hobby"
+
+x = txt.count("eat")
+
+print(x)
